fe68f47f2c6744d52015dee94c3e7c534e29939f
RubyArt <ruby_art25@yahoo.com.mx>
2019-08-28 03:14:54 -0500

    BugSize_V5.2
    -Menú con imágenes
    -Mapa con fondo
    -Modifiqué algunas plataformas 
    -Aumenté un poco la fuerza de salto y velocidad de Leslie

---
+++ bugSizeV2.2/Assets/MusicaBillieJean.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaBillieJean : MonoBehaviour
+{
+
+    public AkEvent BillieJean;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wwise_BillieJean();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Wwise_BillieJean()
+    {
+        if (BillieJean != null)
+        {
+            BillieJean.HandleEvent(gameObject);
+        }
+    }
+}
