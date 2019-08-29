bfb281a93342cc6aff5001c64cac36ff79d21b8a
Alexis <alex.tover@gmail.com>
2019-08-29 03:21:11 -0500

    BugSize_V7.1
    -Los curados ya no chocan con el personaje y siguen a los enemigos
    -Ya hay picos que lastiman al jugador
    -Ya hay en prefab corazones que regeneran la vida del jugador, la cantidad que regeneran se puede modificar sin codigo
    (:D) ya casi queda bien esta cosa \m/

---
+++ bugSizeV2.2/Assets/Scripts/Leslie_Scripts/txt_Corazones.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class txt_Corazones : MonoBehaviour
+{
+    public int corazonTaken;
+
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            player_Life player = col.GetComponent<player_Life>();
+            if (player != null)
+            {
+                Debug.Log("Healed");
+                player.TakeCorazon(corazonTaken);
+                Destroy(gameObject);
+
+            }
+        }
+
+    }
+
+}
